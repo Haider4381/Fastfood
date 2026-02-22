0b26321d69ca41663908ed6af80464c995c7279a
Haider4381 <haidermehar1222@gmail.com>
2026-02-22 11:25:58 +0500

    Delete app directory

--- app/logout.js
+++
@@ -1,6 +0,0 @@
-// Logout
-on(ui.logoutBtn, 'click', async () => {
-  try { await apiFetch('../api/logout', { method: 'POST' }); } catch (_){}
-  try { localStorage.removeItem('ffpos_user'); localStorage.removeItem('ffpos_last_order_id'); } catch(_){}
-  goPublicRoot();
-});
